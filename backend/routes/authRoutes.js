5e1272f4ddc08dc58260bfdea1b63c8f67f576ca
mahija-solvingclub <mahireddyofficial123@gmail.com>
2026-05-23 11:29:19 +0530

    fixed auth token

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -10,7 +10,8 @@ const generateToken = (res, userId) => {
   res.cookie('jwt', token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === 'production',
-    sameSite: 'lax',
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax',
+    path: '/',
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
   return token;
